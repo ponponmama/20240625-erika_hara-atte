afd5958f07f04ef1f798150fa5b8b38159bcb874
ponponmama <buti1024@gmail.com>
2025-03-13 11:20:06 +0900

    setup.sh作成

---
+++ src/setup.sh
@@ -0,0 +1,12 @@
+mkdir -p src/bootstrap/cache \
+        src/storage \
+        src/storage/framework/cache \
+        src/storage/framework/cache/data \
+        src/storage/framework/sessions \
+        src/storage/framework/testing \
+        src/storage/framework/views \
+        src/storage/logs \
+        src/storage/logs/app \
+        src/storage/logs/app/public
+
+echo "ディレクトリが正常に作成されました。"
